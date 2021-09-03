e707be63cc1e1257d5aa6e44c077f7ae02430c23
Pavan <pavans@geekyants.com>
2021-09-03 20:52:35 +0530

    header | footer

---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<header>
+    <h1>
+        <img src="/img/logo.png" alt="Poll logo">
+    </h1>
+</header>
+
+<style>
+    header {
+        background: #f7f7f7;
+        padding: 20px;
+    }
+
+    h1{
+        margin: 0;
+        text-align: center;
+    }
+</style>
