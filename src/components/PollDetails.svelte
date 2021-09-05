7a14a633aef6afb2c61885d820e4902f145c78b0
Pavan <pavans@geekyants.com>
2021-09-05 16:00:35 +0530

    tweened

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { tweened } from 'svelte/motion';
     import PollStore from '../store/PollStore';
     import Card from '../shared/Card.svelte';
     import Button from '../shared/Button.svelte';
@@ -9,6 +10,12 @@
     $: totalVotes = poll.votesA + poll.votesB;
     $: percentA = Math.floor(100 / totalVotes * poll.votesA);
     $: percentB = Math.floor(100 / totalVotes * poll.votesB);
+
+    // Tweened values
+    const tweenedA = tweened(0);
+    const tweenedB = tweened(0);
+    $: tweenedA.set(percentA);
+    $: tweenedB.set(percentB);
 
     const handleVote = (option, id) => {
         PollStore.update(currentPolls => {
@@ -39,11 +46,11 @@
         <h3>{poll.question}</h3>
         <p>Total Votes: {totalVotes}</p>
         <div class="answer" on:click={() => handleVote('a', poll.id)}>
-            <div class="percent percent-a" style="width: {percentA}%"></div>
+            <div class="percent percent-a" style="width: {$tweenedA}%"></div>
             <span>{poll.answerA} ({poll.votesA})</span>
         </div>
         <div class="answer" on:click={() => handleVote('b', poll.id)}>
-            <div class="percent percent-b" style="width: {percentB}%"></div>
+            <div class="percent percent-b" style="width: {$tweenedB}%"></div>
             <span>{poll.answerB} ({poll.votesB})</span>
         </div>
         <Button flat={true} on:click={() => handleDelete(poll.id)}>Delete</Button>
